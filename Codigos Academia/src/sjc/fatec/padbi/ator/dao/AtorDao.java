dc42e0e622b5b0fa09cba97f557f8a9f8c5883e6
Edson Akira Ito <edson.akira21@gmail.com>
2014-05-28 15:26:10 -0300

    modificações finais

--- Codigos Academia/src/sjc/fatec/padbi/ator/dao/AtorDao.java
+++
@@ -1,8 +0,0 @@
-package sjc.fatec.padbi.ator.dao;
-
-import sjc.fatec.padbi.ator.model.Ator;
-import sjc.fatec.padbi.ator.model.Login;
-
-public interface AtorDao {
-	public Ator logar(Login login);
-}
--- Codigos Academia/src/sjc/fatec/padbi/funcionario/dao/FuncionarioDao.java
+++
@@ -1,9 +0,0 @@
-package sjc.fatec.padbi.funcionario.dao;
-
-import java.io.Serializable;
-
-import sjc.fatec.padbi.funcionario.model.Funcionario;
-
-public interface FuncionarioDao {
-	public Serializable cadastrar(Funcionario funcionario);
-}
